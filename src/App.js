68787ad72d9de76270cc1fa3655526a69daca434
Manjula <ssmanjulasv91@gmail.com>
2022-10-05 23:47:44 +0530

    header component updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>
+          <ul className="nav">
+            <li>Tutorials</li>
+            <li>References</li>
+            <li>Exercises</li>
+            <li>Videos</li>
+          </ul>
+          <ul className="nav2">
+            <li style={{ backgroundColor: "black", color: "white" }}>Pro</li>
+            <li style={{ backgroundColor: "pink" }}>Get Certified</li>
+            <li style={{ backgroundColor: "yellow" }}>Free website</li>
+            <li style={{ backgroundColor: "green", color: "white" }}>Log in</li>
+          </ul>
+        </header>
     </div>
   );
 }
